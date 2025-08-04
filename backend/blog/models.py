7122a9a40cbfba61e6c83a025ad73637b4fec2c5
AdaDevSchool <adadevsschool@gmail.com>
2025-08-04 11:22:25 -0300

    Configuracion 'blog'

---
+++ backend/blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings # Configuracion para el modelo de usuario
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    content = models.TextField(verbose_name="Contenido")
+    published_date = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Publicación")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, # Referencia a tu modelo de usuario personalizado
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+        verbose_name="Autor"
+    )
+
+    class Meta:
+        verbose_name = "Entrada de Blog"
+        verbose_name_plural = "Entradas de Blog"
+        ordering = ['-published_date'] # Ordena los posts por fecha de forma descendente
+
+    def __str__(self):
+        return self.title
---
+++ backend/blog/serializers.py
@@ -0,0 +1,12 @@
+# blog/serializers.py
+from rest_framework import serializers
+from .models import Post
+from usuarios.serializers import CustomUserSerializer
+
+class PostSerializer(serializers.ModelSerializer):
+    author = CustomUserSerializer(read_only=True) # Revisar s ies correcto - esto sirve para que el aauto sea el usuairio  logueado pero esa no es la idea.
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'published_date', 'author']
+        read_only_fields = ['author']
