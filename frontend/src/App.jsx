546a491f0caff1729e104e094f88be8c37252667
AdaDevSchool <adadevsschool@gmail.com>
2025-08-14 15:53:38 -0300

    Actualizaciones generales

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,10 @@ import Courses from './components/Courses';
 import Classes from './components/Classes';
 import Blog from './components/Blog';
 import Pricing from './components/Pricing';
+import FeaturesSection from './components/FeaturesSection';
+import TestimonialsSection from './components/TestimonialsSection';
+import FinalCTA from './components/FinalCTA';
+import Footer from './components/Footer';
 // Importa tus archivos CSS aquí
 import './static/css/main.css'
 import './static/css/main.css';
@@ -24,6 +28,11 @@ const App = () => {
             <Classes />
             <Blog />
             <Pricing />
+            <FeaturesSection />
+            <TestimonialsSection />
+            <FinalCTA />
+            <Footer />
+            
             {/* Si tienes un footer, iría aquí */}
         </div>
     );
