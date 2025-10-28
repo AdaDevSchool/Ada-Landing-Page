4dd00dd8e58929dd34ff3ed47927640fc196bd03
AdaDevsSchool <adadevsschool@gmail.com>
2025-10-28 11:42:28 -0300

    Actualizaciones

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Ada-Landing-Page
 Landing Page Basica Academia
+
+# Configuraciones + Instalaciones
+
+npm run dev -- incio proyecto en desarrollo
+localhost:3500
--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -5,12 +5,12 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+
+Resumen : ASGI . Estandar moderno para conectar servidores web con aplicaciones Djangosd
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = get_asgi_application()
